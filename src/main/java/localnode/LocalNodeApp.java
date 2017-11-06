87e50d328430c60aba7c16e96a54a60b4fada8a2
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-11-05 21:39:28 -0800

    local node main

--- src/main/java/localnode/LocalNodeApp.java
+++ src/main/java/localnode/LocalNodeApp.java
@@ -10,4 +10,8 @@ public class LocalNodeApp extends Application<LocalNodeConfig>{
         final LocalNodeResource localNodeResource = new LocalNodeResource();
         environment.jersey().register(localNodeResource);
     }
+
+    public static void main(String[] args) throws Exception {
+        new LocalNodeApp().run(args);
+    }
 }
